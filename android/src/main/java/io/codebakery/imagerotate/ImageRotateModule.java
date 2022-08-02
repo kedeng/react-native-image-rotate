4fbab56f46b349fb06093996a6f1a1e4827e4abd
VictorAugustoDn <55756904+VictorAugustoDn@users.noreply.github.com>
2022-08-02 14:32:27 -0300

    update from jpeg to png

--- android/src/main/java/io/codebakery/imagerotate/ImageRotateModule.java
+++ android/src/main/java/io/codebakery/imagerotate/ImageRotateModule.java
@@ -317,7 +317,7 @@ public class ImageRotateModule extends ReactContextBaseJavaModule {
         if ("image/webp".equals(mimeType)) {
             return ".webp";
         }
-        return ".jpg";
+        return ".png";
     }
 
     private static Bitmap.CompressFormat getCompressFormatForType(String type) {
@@ -327,7 +327,7 @@ public class ImageRotateModule extends ReactContextBaseJavaModule {
         if ("image/webp".equals(type)) {
             return Bitmap.CompressFormat.WEBP;
         }
-        return Bitmap.CompressFormat.JPEG;
+        return Bitmap.CompressFormat.PNG;
     }
 
     private static void writeCompressedBitmapToFile(Bitmap rotated, String mimeType, File tempFile)
